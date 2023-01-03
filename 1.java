6a493cebb5845a0b2203ff19c8845760f8926462
Erlein Nicole <erleinureta@gmail.com>
2023-01-03 10:42:09 +0800

    First Java Practice

---
+++ 1.java
@@ -0,0 +1,9 @@
+/*Types, Variables and Operators*/
+
+class Hello3{
+    public static void main(String[] args){
+        String text = "Hello" + " World";
+        text = text + " Erlein " + 5;
+        System.out.println(text);
+    }
+}
