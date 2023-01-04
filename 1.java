862e4b783f80470c8e8551020feeac52e4d55735
Erlein Nicole <erleinureta@gmail.com>
2023-01-04 10:43:22 +0800

    Jan 4 2023 java practice

--- 1.java
+++
@@ -1,9 +0,0 @@
-/*Types, Variables and Operators*/
-
-class Hello3{
-    public static void main(String[] args){
-        String text = "Hello" + " World";
-        text = text + " Erlein " + 5;
-        System.out.println(text);
-    }
-}
---
+++ BroCode Practice LOL/Main.java
@@ -0,0 +1,80 @@
+//These are practice codes from youtube and various sources//
+
+/*Types, Variables and Operators*/
+/** 
+class swap{
+    public static void main(String[] args){
+        String x = "water";
+        String y = "KoolAid";
+        String temp;
+
+        temp = x;
+        x = y;
+        y = temp;
+
+        System.out.println("x: "+x);
+        System.out.println("y: "+y);
+    }
+}
+*/
+
+//I/o hackerrank
+/** 
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int c = scanner.nextInt();
+        scanner.close();
+        
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+    }
+}
+
+**/
+//I/o 2 hackerrank
+/** 
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int lastLine = scanner.nextInt();
+        double secondLine = scanner.nextDouble();
+        scanner.nextLine();
+        String firstLine = scanner.nextLine();
+        scanner.close();
+
+        System.out.println("String: " + firstLine);
+        System.out.println("Double: " + secondLine);
+        System.out.println("Int: " + lastLine);
+    }
+}
+**/ 
+
+//Java GUI
+/**
+import javax.swing.JOptionPane;
+
+public class Main {
+ 
+    public static void main(String[] args) {
+        String name = JOptionPane.showInputDialog("Enter your name");
+        JOptionPane.showMessageDialog(null,"Hello " + name);
+
+        int age = Integer.parseInt(JOptionPane.showInputDialog("Enter your age"));
+        JOptionPane.showMessageDialog(null,"You are " + age + " years old.");
+
+        double weight = Double.parseDouble(JOptionPane.showInputDialog("Enter your weight(kg)"));
+        JOptionPane.showMessageDialog(null,"You weigh " + weight + " kg");
+    }
+} 
+
+**/
