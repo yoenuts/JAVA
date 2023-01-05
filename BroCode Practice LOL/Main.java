925afc3df6d9025c441d4369139069589a76df08
Erlein Nicole <erleinureta@gmail.com>
2023-01-05 23:16:14 +0800

    CodingBat warmup1 Sol

--- BroCode Practice LOL/Main.java
+++ BroCode Practice LOL/Main.java
@@ -126,3 +126,13 @@ public class Main{
 }
 
 **/
+public class Main{
+    public int diff21(int n) {
+        if(n > 21){
+          return (21 - n) * -2;
+        }
+        else {
+            return 21 - n;
+        } 
+    }
+}
---
+++ CodingBat/WarmUp.java
@@ -0,0 +1,11 @@
+//Theae are my solution to Coding Bat. I'm not necessarily sure if the syntax is correct since I havent gotten so far as to dive into proper syntax for functions.
+public class WarmUp {
+    public int diff21(int n){
+        if(n > 21){
+            return (21 - n) * 2;
+        }
+        else{
+            return 21 - n;
+        }
+    }
+}
