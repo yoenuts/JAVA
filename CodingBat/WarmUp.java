5aa5c41eee4af118bd094e69a7c3daace64f9408
Erlein Nicole <erleinureta@gmail.com>
2023-01-10 22:42:38 +0800

    nauurat ako

--- CodingBat/WarmUp.java
+++ CodingBat/WarmUp.java
@@ -1,3 +1,4 @@
+
 //Theae are my solution to Coding Bat. I'm not necessarily sure if the syntax is correct since I havent gotten so far as to dive into proper syntax for functions.
 /**public class WarmUp {
     public static int diff21(int n){
@@ -10,6 +11,54 @@
     }
 }**/
 
+//practicing functions and stuff
+/**
+import java.util.Scanner;
+public class Main{
+    public static void main(String[] args){
+    
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter a value: ");
+        int n = scan.nextInt();
+        scan.close();
+        nearHundred(n);
+    }
+    public static boolean nearHundred(int value){
+        if ((Math.abs(100 - value) <= 10) || (Math.abs(200 - value) <= 10)) {
+            System.out.println("Yo");
+            return true;
+        }
+        else{
+            System.out.println("Mama");
+            return false;
+        }
+    }
+}
+**/
+/** 
+// at this point i dont et why i have to include return just so i can print out 
+import java.util.Scanner;
+public class Main{
+    public static void main(String[] args){
+        
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter a word:");
+        String word = new String(scan.nextLine());
+        scan.close();
+        notString(word);
+    }
+
+    public static String notString(String str){
+        if (str.length() >= 3 && str.substring(0, 3).equals("not")) {
+            System.out.println(str);
+            return str;
+        }
+        System.out.println("not "+str);
+        return "not" +str;
+    }
+}
+**/
+/** 
 import java.util.Scanner;
 public class WarmUp{
     public static void main(String[] args){
@@ -33,3 +82,27 @@ public class WarmUp{
         }
     }
 }   
+**/
+
+import java.util.Scanner;
+public class WarmUp{
+    public static void main(String[] args){
+        
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter a word:");
+        String word = new String(scan.nextLine());
+        scan.close();
+        front3(word);
+    }
+    //exchange the first and last letters
+    public static String front3(String str) {
+        String first = str.substring(0, str.length());
+        if (str.length() >= 3){
+            first = str.substring(0, 3);
+        }
+
+        System.out.println(first + first + first);
+        return first + first + first;
+
+    }
+} 
