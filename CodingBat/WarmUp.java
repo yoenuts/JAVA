20c3f6f3832b9c18d6bd5dd7226e26607de6018f
Erlein Nicole <erleinureta@gmail.com>
2023-01-11 21:53:16 +0800

    one liner

--- CodingBat/WarmUp.java
+++ CodingBat/WarmUp.java
@@ -224,3 +224,14 @@ public class WarmUp{
       }
     **/
 }
+
+
+/**
+public boolean or35(int n) {
+  if (n % 3 == 0 || n % 5 == 0){
+    return true;
+  }
+  return false;
+}
+// return (n % 3 == 0) || (n % 5 == 0);
+**/
