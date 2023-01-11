ca15f859658eacdefa92cca998cf56f88106a4a5
Erlein Nicole <erleinureta@gmail.com>
2023-01-11 21:42:08 +0800

    new examples

--- CodingBat/WarmUp.java
+++ CodingBat/WarmUp.java
@@ -135,9 +135,9 @@ public class WarmUp{
 
 **/
 
-/**
 import java.util.Scanner;
 //simple return the highest value program
+/**
 public class WarmUp {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
@@ -178,5 +178,37 @@ public class WarmUp {
         return c;
         **/
     }
-}
 **/
+
+public class WarmUp{
+    public static void main(String[] args){
+        
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter a word:");
+        String word = new String(scan.nextLine());
+        scan.close();
+        endUp(word);
+    }
+    //capitalize the last three letters
+    public static String endUp(String str) {
+        String front, back;
+        if(str.length() >= 3) {
+          back = str.substring(str.length() - 3, str.length());
+          front = str.substring(0, str.length() - 3);
+          return front + back.toUpperCase();
+        }
+        return str.toUpperCase();
+    }
+    
+    //other way of doing so
+      /**
+       public String endUp(String str) {
+        if (str.length() <= 3) return str.toUpperCase();
+        int cut = str.length() - 3;
+        String front = str.substring(0, cut);
+        String back  = str.substring(cut);  // this takes from cut to the end
+        
+        return front + back.toUpperCase();
+      }
+    **/
+}
