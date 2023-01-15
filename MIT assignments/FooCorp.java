6698de6a8575c8c3b495806119b192b181b36612
Erlein Nicole <erleinureta@gmail.com>
2023-01-15 09:09:30 +0800

    FooCorp COmplete :)

--- MIT assignments/FooCorp.java
+++ MIT assignments/FooCorp.java
@@ -10,36 +10,34 @@ import java.util.Scanner;
 
 public class FooCorp {
     public static void main(String[] args) {
+        double base_pay;
+        double workhours;
         Scanner scan = new Scanner(System.in);
         System.out.println("Enter your name:");
         String name = scan.nextLine();
         System.out.println("Hello, "+name);
-        System.out.println("Enter number of hours worked:");
+        
+        do{
+            System.out.println("What is Employee's Base Pay?(Min. of $8)");
+            base_pay = scan.nextDouble();
+        }while(base_pay < 8);
 
-        double workhours = scan.nextInt();
-        while(workhours > 60.0) {
-            System.out.println("You're overworking them pay million dollars. Try Again.");
-            break;
-        }
- 
-        System.out.println("What is Employee's Base Pay?");
-
-        double base_pay = scan.nextDouble();
-        while(base_pay < 8.0) {
-            System.out.println("Modern day slavery?! Try again.");
-            break;
-        }
+        do{
+            System.out.println("Enter number of hours: ");
+            workhours = scan.nextDouble();
+        }while(workhours < 40 || workhours > 60);
 
         scan.close();
 
-        FooCorp(workhours,base_pay);
+        fooCorp(workhours,base_pay);
     }
 
-    public static double FooCorp(double hours, double pay) {
+    public static double fooCorp(double hours, double pay) {
         double result = hours * pay;
         if(hours > 40){
             result = hours * (pay * 1.5) ;
         }
+
         System.out.println("Pay your Employee: $" +result);
         return result;
     }
