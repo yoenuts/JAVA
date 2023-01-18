644b9c88cbb9c3b4a6233c777e88268e8505d93a
Erlein Nicole <erleinureta@gmail.com>
2023-01-18 21:00:02 +0800

    new commit

--- CodingBat/WarmUp2.java
+++ CodingBat/WarmUp2.java
@@ -181,3 +181,11 @@ public int array667(int[] nums) {
     return count;
 }
 **/
+/**
+public boolean has271(int[] nums) {
+  for(int i = 0; i < nums.length - 2; i++) {
+    if(nums[i + 1] == nums[i]+ 5 && Math.abs(nums[i+2] - (nums[i] - 1)) <= 2) return true;
+  }
+  return false;
+}
+ */
---
+++ MIT assignments/arrays.java
@@ -0,0 +1,7 @@
+class arrays{
+    public static void main(String[] args) {
+
+    }
+
+    public static int 
+}
