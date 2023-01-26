72d3e8300640f068b71c06de0970e7ac7033938a
Erlein Nicole <erleinureta@gmail.com>
2023-01-26 21:05:37 +0800

    implement methods

--- MIT assignments/arrays.java
+++ MIT assignments/arrays.java
@@ -28,3 +28,32 @@ public int[] reverse3(int[] nums) {
   return Array;
 }
  */
+/* 
+ //this is my solution. i though about using math max but somehow didnt cuz i thought i'd need to implement a loop?
+
+ public int[] maxEnd3(int[] nums) {
+    int[] Array = new int [3];
+    for(int i = 0; i < nums.length; i++) {
+      if(nums[0] > nums[nums.length - 1]) {
+        Array[i] = nums[0];
+      }
+      else {
+        Array[i] = nums[nums.length - 1];  
+      }
+    }
+    
+    return Array;
+  }
+// this part is their solution
+  public int[] maxEnd3(int[] nums) {
+    int max = Math.max(nums[0], nums[2]);
+    nums[0] = max;
+    nums[1] = max;
+    nums[2] = max;
+    return nums;
+    
+    // Solution notes: you could write if-logic to figure out
+    // which element is the biggest, but here we use Math.max()
+    // to solve that part nicely.
+  }
+*/
