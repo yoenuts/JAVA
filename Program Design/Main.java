6ef9c84e2c6766d57f583fc2907db3c5da2d4248
Erlein Nicole <erleinureta@gmail.com>
2023-02-01 11:13:01 +0800

    Circle calculator

--- Program Design/Main.java
+++ Program Design/Main.java
@@ -53,15 +53,39 @@ public class Main{
 //HAHHAHA ANG SAYA OK
 
 import javax.swing.JOptionPane;
-
+/*
 public class Main{
     public static void main(String[] args) {
         String name = JOptionPane.showInputDialog("Enter your name and press OK.");
         JOptionPane.showMessageDialog(null, "Hello " + name, "Welcome", JOptionPane.PLAIN_MESSAGE);
 
         //information, warning, plain and question
+    }
+}
+*/
+//Circle Calculator
 
+public class Main{
+    public static void main(String[] args) {
+        double radius;
+        double area;
+        double circumference;
 
+        String radiusString;
+        String outputStr;
+
+        radiusString = JOptionPane.showInputDialog("Enter the radius: ");
+
+        radius = Double.parseDouble(radiusString);
+        area = Math.PI * radius * radius;
+        circumference = 2 * Math.PI * radius;
+
+        outputStr = "Radius: " + radius + "\n" +
+            "Area: " + area + "square units\n" +
+            "Circumference: " + circumference + " units";
+
+        JOptionPane.showMessageDialog(null, outputStr, "Circle", JOptionPane.INFORMATION_MESSAGE);
+        System.exit(0); 
     }
 }
 
