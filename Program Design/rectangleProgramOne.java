a10dd442dd66b76173a87ef0a3f78420af609064
Erlein Nicole <erleinureta@gmail.com>
2023-02-09 21:22:18 +0800

    rectangle calc

--- Program Design/rectangleProgramOne.java
+++ Program Design/rectangleProgramOne.java
@@ -1,14 +1,19 @@
 //first java frame program!
+//NOTES
 
+//1.) program contains one class, rectangleProgram
+//2.) it has constructor rectangleProgram
+//3.) created this class based on JFrame therefore this program is a subclass of the super class JFrame.
+//4.)
 import javax.swing.*;
 
-public class rectangleProgram extends JFrame {
+public class rectangleProgramOne extends JFrame {
     // there are set to private because
     // we want them to be used only for this class
     private static final int WIDTH = 400;
     private static final int HEIGHT = 300;
 
-    public rectangleProgram() {
+    public rectangleProgramOne() {
         setTitle("Area and Perimeter of a Rectangle");
         setSize(WIDTH, HEIGHT);
 
@@ -17,6 +22,6 @@ public class rectangleProgram extends JFrame {
     }
 
     public static void main(String[] args) {
-        rectangleProgram rectProg = new rectangleProgram();
+        rectangleProgramOne rectProg = new rectangleProgramOne();
     }
 }
