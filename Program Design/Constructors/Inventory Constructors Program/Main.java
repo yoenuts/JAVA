f4d32c91a9c04de7da54d25be8957770b72b4fc7
Erlein Nicole <erleinureta@gmail.com>
2023-03-06 15:04:02 +0800

    radiusconstprog

---
+++ Program Design/Constructors/Inventory Constructors Program/Main.java
@@ -0,0 +1,12 @@
+/**
+ * Main
+ */
+public class Main {
+    public static void main(String[] args) {
+        
+        Inventory milk = new Inventory("Cowhead");
+
+        System.out.println(milk.name);
+
+    }
+}
