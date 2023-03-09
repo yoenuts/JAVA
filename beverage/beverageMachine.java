33aa1cd1b9e89c5144a14ccf3991686bcfeeb84d
Erlein Nicole <erleinureta@gmail.com>
2023-03-09 10:56:48 +0800

    have to fix error but done

--- beverage/beverageMachine.java
+++ beverage/beverageMachine.java
@@ -1,18 +1,24 @@
 package beverage;
 import java.util.*;
 import java.awt.*;
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JOptionPane;
-import javax.swing.SwingConstants;
-
+import javax.swing.*;
 
 // ugyyu
 public class beverageMachine extends JFrame {
     private JLabel headingMainL;
     private JLabel selectItem;
     private JButton sodaB, juiceB, energyB, coffeeB, exitB;
+    //ButtonHandler
+    private ButtonHandler pbutton;
+    //main method stock sum stuff
+    public static void main(String[] args){
+        CashRegister theCash = new CashRegister(1000);
+        Dispenser Soda = new Dispenser(40, 20);
+        Dispenser Coffee = new Dispenser(20, 15);
+        Dispenser energyDrink = new Dispenser(20, 25);
+        Dispenser juice = new Dispenser(50, 10);
+        beverageMachine run = new beverageMachine();
+    }
 
     public beverageMachine() {
         //create the labels
@@ -25,6 +31,12 @@ public class beverageMachine extends JFrame {
         energyB = new JButton("Energy Drink");
         coffeeB = new JButton("Coffee");
         exitB = new JButton("Exit");
+        //create listeners
+        sodaB.addActionListener(pbutton);
+        juiceB.addActionListener(pbutton);
+        energyB.addActionListener(pbutton);
+        coffeeB.addActionListener(pbutton);
+        exitB.addActionListener(pbutton);
 
         //create a container
         Container contain = getContentPane();
@@ -48,6 +60,7 @@ public class beverageMachine extends JFrame {
         double coinsInserted = 0;
         double price;
         double coinsRequired;
+        double change = 0;
         String str;
 
         //check if it's not empty
@@ -57,28 +70,62 @@ public class beverageMachine extends JFrame {
             while (coinsRequired > 0) {
                 str = JOptionPane.showInputDialog("To buy " + productName + " please insert " + coinsRequired);
                 coinsInserted = coinsInserted + Integer.parseInt(str);
-                coinsRequired = price - coinsInserted;
                 //give change if coin inserted is greater than the cost
                 if(coinsInserted > coinsRequired) {
-                    run
+                    change = coinsInserted - coinsRequired;
+                    str = String.format("Your change is %.2f", change);
+                    JOptionPane.showMessageDialog(null, str, "Change", JOptionPane.INFORMATION_MESSAGE);
+                    coinsInserted = coinsInserted - change;
                 }
+                coinsRequired = price - coinsInserted;
             }
+            //idk why this is showing an error
+            theCash.acceptMoney(coinsInserted);
+            product.makeSale();
+
+            str = "Enjoy your " + productName + "!";
+
+            JOptionPane.showMessageDialog(null, str, "Purchase Succesful", JOptionPane.PLAIN_MESSAGE);
+        }
+        //dispenser is empty
+        else{
+            str = "Sorry, " + productName + " is sold out.";
+            JOptionPane.showMessageDialog(null, str, "Purchase Unsuccesful", JOptionPane.PLAIN_MESSAGE);
+
         }
     }
 
-
-
     //now i gotta create a listener for all that button UGH
-    
-    //main method stock sum stuff
-    public static void main(String[] args){
-        CashRegister cashRegister = new CashRegister(1000);
-        Dispenser Soda = new Dispenser(40, 20);
-        Dispenser Coffee = new Dispenser(20, 15);
-        Dispenser energyDrink = new Dispenser(20, 25);
-        Dispenser juice = new Dispenser(50, 10);
-        beverageMachine run = new beverageMachine();
+     
+    /**
+     * InnerbeverageMachine
+     */
+
+    // formal parameter e is a reference variable to ActionEventtype The class ActionEvent
+    //contains getActionCommand(YOHOO FINALLY UNDERSTAND Y THEY USE THIS)
+    //used to identify which button generated the event.
+    public class ButtonHandler implements ActionListener {
+        public void ActionPerformed(ActionEvent e) {
+            if(e.getActionCommand.equals("Exit")){
+                System.exit(0);
+            }
+            else if(e.getActionCommand.equals("Coffee")){
+                sellProduct(Coffee, "Coffee");
+            }
+            else if(e.getActionCommand.equals("Juice")){
+                sellProduct(juice, "Juice");
+            }
+            else if(e.getActionCommand.equals("Energy Drink")){
+                sellProduct(energyDrink, "Energy Drink");
+            }
+            else if(e.getActionCommand.equals("Soda")){
+                sellProduct(Soda, "Soda");
+            }
+
+        }
+        
     }
+    
 
     
 
